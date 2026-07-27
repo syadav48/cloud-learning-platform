29a7272323dd73d88e1577b8c0375219d9f1a78a
syadav48 <suryayadav181194@gmail.com>
2026-07-27 20:17:45 +0530

    Lamda generator and handler addition - 3

--- src/main/java/handler/ThumbnailHandler.java
+++ src/main/java/handler/ThumbnailHandler.java
@@ -2,21 +2,28 @@ package handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import java.util.HashMap;
-import java.util.Map;
 
-public class ThumbnailHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+
+public class ThumbnailHandler implements RequestHandler<S3Event, String> {
 
     @Override
-    public Map<String, String> handleRequest(Map<String, String> input, Context context) {
-        // Extract "name" from input JSON
-        String name = input.getOrDefault("name", "World");
+    public String handleRequest(S3Event s3event, Context context) {
+        context.getLogger().log("Lambda Started");
 
-        // Build response
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello " + name + " from AWS Lambda");
+        // Iterate over S3 event records
+        for (S3EventNotification.S3EventNotificationRecord record : s3event.getRecords()) {
+            String bucket = record.getS3().getBucket().getName();
+            String key = record.getS3().getObject().getKey();
 
-        return response;
+            context.getLogger().log("Received S3 Event - Bucket: " + bucket + ", Key: " + key);
+            String eventName = record.getEventName();
+            // Here you can add your thumbnail generation logic
+            // For now, just log and return a message
+            context.getLogger().log("eventName: {}" + eventName);
+        }
+
+        return "S3 Event processed successfully";
     }
 }
-
